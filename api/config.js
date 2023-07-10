4cf5cb1d41d50cb001a930a6928fede018658b03
Sarah Kimball <sarahkimballpc@gmail.com>
2023-07-10 17:47:54 -0400

    Y

---
+++ api/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  APP_ID: "e72ce815",
+  API_KEY: "7d19dc777c314245a601e198cfa23ecb",
+  BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
+  BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application/json',
+};
+
